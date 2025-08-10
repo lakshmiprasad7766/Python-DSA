369e268b00b0aa3cc25ac390a398131900298d10
Prasad <lakshmiprasad7766@gmail.com>
2025-08-11 00:12:44 +0530

    string topic is committed

--- Basic.py
+++ Basic.py
@@ -1,23 +1,42 @@
-# Hi this is first basic python code to push into GitHub Repository
+# # Hi this is first basic python code to push into GitHub Repository
 
-#print("Hello Github, this is the first commit from my local machine")
+# #print("Hello Github, this is the first commit from my local machine")
 
-print("Python coding for learning")
+# print("Python coding for learning")
 
-print(2+3)
+# print(2+3)
 
 
-# Variables
-x = 2
-y = 4
+# # Variables
+# x = 2
+# y = 4
  
-print("Sum of x and y is: ", x + y)
+# print("Sum of x and y is: ", x + y)
 
 
-x=9
-print("New value of x is: ", x)
+# x=9
+# print("New value of x is: ", x)
 
 
 #string
 name = "Prasad Tech"
 print("Name is: ", name)
+
+
+#Concatenation
+name + 'Channel'
+print("Updated Name is: ", name + ' Channel')
+
+
+# string reversal
+reversed_name = name[::-1]
+print("Reversed Name is: ", reversed_name)
+
+
+
+#print (name[1:5])
+
+print('my name is ' + name[7:])
+
+print(len(name))
+
