f154261f9418b19bfa40282eff2cd02a696bd6eb
Prasad <lakshmiprasad7766@gmail.com>
2025-08-10 23:56:01 +0530

    python basics demo

--- Basic.py
+++ Basic.py
@@ -1,7 +1,23 @@
 # Hi this is first basic python code to push into GitHub Repository
 
-print("Hello Github, this is the first commit from my local machine")
+#print("Hello Github, this is the first commit from my local machine")
 
-print("latest changes")
+print("Python coding for learning")
 
 print(2+3)
+
+
+# Variables
+x = 2
+y = 4
+ 
+print("Sum of x and y is: ", x + y)
+
+
+x=9
+print("New value of x is: ", x)
+
+
+#string
+name = "Prasad Tech"
+print("Name is: ", name)
